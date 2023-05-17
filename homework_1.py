38c7ca4a1c53d6c901cd23b093c4843f65fa9f6e
Sergei Eremkin <ffr@list.ru>
2023-05-17 15:05:11 +0300

    добавил сообщение при проигрыше в последнем задании

--- homework_1.py
+++ homework_1.py
@@ -57,22 +57,27 @@ def simple():
 
 def guess_number():
     LOWER_LIMIT = 0
-    UPPER_LIMIT = 1000
-    COUNT_TRY = 10
+    UPPER_LIMIT = 100
+    COUNT_TRY = 2
     RAND_NUMBER = randint(LOWER_LIMIT, UPPER_LIMIT)
+    is_win = True
     for _ in range(COUNT_TRY + 1):
         number = int(input('Введи число от 0 до 1000: '))
         if number > RAND_NUMBER:
             print('Ваше число БОЛЬШЕ загаданного')
+            is_win = False
         elif number < RAND_NUMBER:
             print('Ваше число МЕНЬШЕ загаданного')
+            is_win = False
         else:
             print('Вы выиграли!')
+            is_win = True
+    if not is_win:
+        print('К сожалению вы проиграли')
 
-
-check_triangle(1, 2, 2)
+#check_triangle(1, 2, 2)
 print()
-simple()
+#simple()
 print()
 guess_number()
 
