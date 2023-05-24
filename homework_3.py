50584ef4c90f2a14651759156344ef3e2a8aa2e3
Sergei Eremkin <ffr@list.ru>
2023-05-24 23:13:39 +0300

    в 3-ем заданиии поменял условие в if

--- homework_3.py
+++ homework_3.py
@@ -64,7 +64,7 @@ def backpack_capacity(capacity: int, stuff: dict) -> list[str]:
     packaging_option = []
     summary = []
     for key, value in stuff.items():
-        if value <= capacity and key not in packaging_option:
+        if value <= capacity:
             capacity -= value
             packaging_option.append(key)
     return packaging_option
